ee43291a857cb1e3c7603027b0c88a00a0a78410
Dr. Shahin Rostami <srostami@bournemouth.ac.uk>
2016-05-19 12:57:41 +0000

    Implemented initilisation of population

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+extern crate rand;
+mod zdt1;
+mod gaussian_mutation;
+use rand::{random, Closed01};
+use zdt1::zdt1;
+use gaussian_mutation::gaussian_mutation;
+
+#[derive(Copy, Clone, Debug)]
+struct Solution {
+    parameters: [f32; 30],
+    objectives: [f32; 2],
+}
+
+fn initialise_population(initial_population: bool) -> [Solution; 100] {
+    let mut population = [Solution {
+        parameters: [0_f32; 30],
+        objectives: [0_f32; 2],
+    }; 100];
+
+    if initial_population {
+        for solution in &mut population[..] {
+            for parameter in &mut solution.parameters[..] {
+                let Closed01(parameter) = random::<Closed01<f32>>();
+            }
+
+            solution.objectives = zdt1(solution.parameters);
+        }
+    }
+
+    return population;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let generations = 10;
+    let mutation_rate = 0.2_f32;
+    let mut parent_population = initialise_population(true);
+    let mut offspring_population = initialise_population(false);
+
+    for gen in 0..generations {
+        for solution in &mut parent_population[..] {
+            solution.parameters = gaussian_mutation(solution.parameters, mutation_rate);
+            solution.objectives = zdt1(solution.parameters);
+        }
+    }
 }
